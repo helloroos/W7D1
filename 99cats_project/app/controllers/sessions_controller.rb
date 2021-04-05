36cc98ef2bf9377a5e383aceec9b1ea5b5b4ea73
michelleroos <hello.michelleroos@gmail.com>
2021-04-05 15:41:03 -0400

    Created sessions_controller.rb and multiple routes

---
+++ 99cats_project/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    def new
+        render :new
+    end
+
+    def create
+        user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user.nil?
+            flash.now[:errors]=['Incorrect username and/or password']
+            render :new
+        else
+            login_user!(user)
+            redirect_to user_url(user)
+        end
+    end
+
+    def destroy
+        
+    end
+
+end
